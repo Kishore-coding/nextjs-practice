cbb97b293770c2f52aede13fecca960e297e9d14
kishore <kishore@starberry.tv>
2023-09-17 13:27:25 +0530

    removed unwanted code

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -4,7 +4,6 @@ import Button from "react-bootstrap/Button";
 import Col from "react-bootstrap/Col";
 import Form from "react-bootstrap/Form";
 import Row from "react-bootstrap/Row";
-import { isMobile } from "react-device-detect";
 
 import "./ContactForm.scss";
 
